56f54171987181492319a7099c1e4da40fb53e4c
gkkary3 <81800956+gkkary3@users.noreply.github.com>
2022-07-05 19:37:27 +0900

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,23 +1,12 @@
 const toggleBtn = document.querySelector('.ham');
 const nav = document.querySelector('.nav-nav');
 const navin = document.querySelector('.nav-in');
-// const navin = document.querySelector('nav-in');
-// const signUpBtn = document.querySelector('.signUp-btn');
-
 toggleBtn.addEventListener('click',()=>{
     nav.classList.toggle('active');
     navin.classList.toggle('active');
-    // signUpBtn.classList.toggle('active');
+
+   
 
     
 });
 
-// toggleBtn2.addEventListener('click',()=>{
-    
-    
-//     seconds.classList.toggle('active');
-//     third.classList.toggle('exit');
-    
-   
-    
-// });
